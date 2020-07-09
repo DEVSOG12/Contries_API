cf9794fe8e5ead08247d00cd783a3672ab5663e9
DEVSOG12 <oreofesolarin@gmail.com>
2020-07-09 04:18:32 +0100

    FIx

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('all_countries.json');
-const route1r = jsonServer.router('countries.json');
+// const route1r = jsonServer.router('countries.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
-server.use(route1r);
+// server.use(route1r);
 
 server.listen(port);
