d8f1af0abca4711dc85c73f2df54fb4e774be572
DEVSOG12 <oreofesolarin@gmail.com>
2020-07-10 18:19:11 +0100

    Done

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('all_countries.json');
+const router = jsonServer.router('countries.json');
 // const route1r = jsonServer.router('countries.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
